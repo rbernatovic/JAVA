d3a192275e17047dd2dfa80ccab0f906ae17f561
rbernatovic <57090199+rbernatovic@users.noreply.github.com>
2019-11-12 10:41:35 +0200

    asd

---
+++ CarRentSystem/src/lt/codeacademy/CarRentSystem.java
@@ -0,0 +1,14 @@
+package lt.codeacademy;
+
+import java.util.*;
+
+public class CarRentSystem {
+
+	public static void main(String[] args) {
+		boolean tiesa = true;
+		System.out.println(tiesa);
+		boolean melas = (5 == 25);
+		System.out.println(melas);
+	}
+
+}
